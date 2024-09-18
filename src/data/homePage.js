5e9b54e674cdf39709d5dc404f052da4e092e9f1
Tim Jedrek <tim@timjedrek.com>
2024-09-17 22:40:31 -0500

    change title

--- src/data/homePage.js
+++ src/data/homePage.js
@@ -1,7 +1,7 @@
 const homePage = {
   data: {
     //SEO Header Stuff
-    pageTitle: "Get More Pilot Students | Right Rudder Marketing",
+    pageTitle: "Enroll More Student Pilots | Right Rudder Marketing",
     pageDescription:
       "Right Rudder Marketing offers career-track, zero-to-hero pilot training programs at Riverside Municipal Airport (KRAL) and Redlands Municipal Airport (KREI). With 320 flying days a year and an FAA-approved Gleim syllabus, our academy provides top-tier education and flexible scheduling options to meet the needs of aspiring pilots. Our programs are designed to ensure high-quality instruction and extensive flying lessons, helping students achieve their goals in flight training efficiently and effectively.",
     pageKeywords:
