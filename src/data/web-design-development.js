205f781fad332791a715ce64ec34d61f32a33fe4
Raul Ospina <raminka13@hotmail.com>
2024-09-17 21:41:36 -0500

    New: web design page

---
+++ src/data/web-design-development.js
@@ -0,0 +1,185 @@
+const webDesign = {
+  data: {
+    //SEO Header Stuff
+    pageTitle:
+      "Web Design & Development for Flight Schools | Right Rudder Marketing",
+    pageDescription:
+      "So what makes a Right Rudder Marketing website different than all the rest? It boils down to these three marketing psychology principles. Using these marketing psychology tactics, your website will convert visitors to actual hot leads.",
+    pageKeywords:
+      "web design, web development, flight school web design, flight school web development, flight school website, flight school website design, flight school website development, flight school website design and development, flight school website design and development services, flight school website design and development company, flight school website design and development agency, flight school website design and development firm, flight school website design and development experts, flight school website design and development professionals, flight school website design and development specialists, flight school website design and development consultants, flight school website design and development services near me, flight school website design and development company near me, flight school website design and development agency near me, flight school website design and development firm near me, flight school website design and development experts near me, flight school website design and development professionals near me, flight school website design and development specialists near me, flight school website design and development consultants near me",
+
+    //top header
+    header: {
+      imagePath: "/src/assets/IMG_0743.jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `More Than </br><span class="text-accent-300">Just a Website</span>`,
+      paragraph:
+        "What makes a Right Rudder Marketing website different than all the rest? It boils down to these three marketing psychology principles. Using these marketing psychology tactics, your website will convert visitors to actual hot leads.",
+      buttons: [
+        {
+          name: "Call Us",
+          link: "tel:1-314-804-1200",
+          primary: false,
+        },
+        {
+          name: "Schedule a Call",
+          link: "/contact",
+          primary: false,
+        },
+      ],
+    },
+
+    //main content
+    builtToConvert: {
+      imagePath: "/src/assets/taylor-van-riper-yQorCngxzwI-unsplash.jpg",
+      imageAlt: "Sky with clouds",
+      heading: "Your website is a sales tool. Are you using it correctly?",
+      upperHeading: "Websites Built to Convert",
+      description: `When you think about designing a website for a flight school, what comes to mind? List our fleet, showcase our instructors, put a contact form and call it done? This is what <strong>91%</strong> of flight school websites do, but by doing this we're missing a huge opportunity.`,
+      paragraphs: [
+        "Your website is your ultimate sales tool in your disposable. A carefully crafted website can attract the right clients, while on the other hand, not investing enough into your website design and website copy will repel your ideal customers.",
+        "At the end of the day, your customers are visiting your website and making a snap judgement about you and your business. If your website is outdated, slow, or not mobile friendly, then you're losing customers.",
+        "So what makes a Right Rudder Marketing website different than all the rest? It boils down to these three marketing psychology principles. Using these marketing psychology tactics, your website will convert visitors to actual hot leads. Let's dive in.",
+      ],
+      list: {
+        heading: "Marketing Psychology Principles",
+        items: [
+          {
+            icon: "M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z",
+            title: "Why Become a Pilot.",
+            descriptions: [
+              "Put Yourself in Their Shoes. When people visit your website, they're on the fence on whether they want to start pilot training.",
+              "Your website needs to show them the advantages of becoming a pilot. They're scouring all of the internet for content and resources to guide them in their decision.",
+              "Become the source of truth for pilot training in your local area. By providing valuable content and resources, you're positioning yourself as the expert in your field.",
+              `Highlight the benefits of becoming a pilot: career aspects, newer freedom, and a new community. This is your chance to answer the visitor's "why".`,
+              `By answering the "why" you're building trust with your potential customers. They're more likely to choose you as their flight school.`,
+            ],
+          },
+          {
+            icon: "m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z",
+            title: "Your Authority.",
+            descriptions: [
+              "Show Them How. Use your website to brandish your authority.",
+              "Lead the visitor step by step on what's the right way to start pilot training through your program.",
+              "Show them your instructors, your fleet, and your facilities. Show them how you're different from the competition.",
+              "Most people are totally confused when first starting the journey in aviation. They're wondering how much it may cost, what the process is, and how to even get started. It's an emotional process to decide to start flight training.",
+              "By showing them the way, you're building trust with your potential customers. They're more likely to choose you as their flight school.",
+            ],
+          },
+          {
+            icon: "M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z",
+            title: "Next Steps.",
+            descriptions: [
+              "Get Their Information",
+              "You need more than just a contact form. While one form is enough to get some leads, Right Rudder Marketing takes it up a notch. We'll use plenty call to actions throughout the site to guide visitors to multiple forms.",
+              "We'll use a variety of forms to get different information from the visitor. This way we can segment the leads and send them targeted emails.",
+              "Some tried and true lead funnels for flight schools include: booking a discovery flight, scheduling a call, and downloading a PDF guide on how to become a pilot.",
+              "Your website is a sales tool to get your visitors information so they can start training at your flight school ASAP.",
+            ],
+          },
+        ],
+      },
+      afterListParagraphs: [
+        "By using these marketing psychology principles, your website will convert visitors to actual hot leads. We've seen it time and time again with our clients. They come to us with a website that's not converting and we turn it into a lead generating machine.",
+        "If you're ready to take your flight school to the next level, then it's time to invest in a website that's built to convert.",
+        `<a class="font-bold hover:underline" href="/contact">Contact us today to get started.</a>`,
+      ],
+    },
+
+    warningCTA: {
+      icon: "M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+      heading: "Don't waste more visitors.",
+      description:
+        "Let us help you build a website that converts visitors to leads. We've helped flight schools across the country increase their leads and grow their business.",
+      button: {
+        text: "Schedule a Call",
+        link: "/contact",
+        primary: true,
+      },
+    },
+
+    offPageSeo: {
+      imagePath: "/src/assets/tadeu-jnr-kAzSa_azENM-unsplash.jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      upperHeading: "Off Page SEO",
+      heading: "Building Credibility, Authority, and Visibility",
+      paragraphs: [
+        "Off page SEO is the process of building links to your website from other websites. The more links you have from other websites, the higher you will rank. This is because search engines like Google, Bing, and Yahoo see these links as a vote of confidence in your website.",
+        "The more votes of confidence you have, the more likely you are to show up when someone types in “Flight School + Your City” into Google. This is why it's important to have a good off page SEO strategy.",
+        "In the flight training industry, it's important to have a good off page SEO strategy. This is because the flight training industry is a competitive one. There are many flight schools out there and they all want to show up on the first page of Google.",
+        "Right Rudder Marketing has a proven strategy that consistently gets our clients to the top of the search engines. We have worked with flight schools in some of the most competitive markets in the United States and have developed a strategy that works.",
+      ],
+      list: {
+        items: [
+          {
+            icon: "M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244",
+            title: "Link building",
+            descriptions: [
+              "Back links are links on other sites that link to your website. Acquiring high quality, authoritative backlinks from reputable websites enhances your flight school's credibility in the eyes of search engines.",
+              "Right Rudder Marketing specializes in identifying relevant industry sources, aviation publications, and influential websites to secure valuable backlinks.",
+              "These backlinks not only drive referral traffic but also signal to search engines that your flight school is a trusted and valuable resource within the flight training community.",
+              "By incorporating a diverse range of backlinks from reputable sources, we can enhance your website's authority and visibility, thereby improving its organic search rankings.",
+            ],
+          },
+          {
+            icon: "M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z",
+            title: "Press release and newswire",
+            descriptions: [
+              "Press releases also play a crucial role in establishing your flight school as a thought leader and industry expert. By distributing well-crafted press releases to reputable news outlets and newswires, you can generate buzz around your flight school and attract media attention.",
+              "By sharing relevant and engaging press releases, we position your flight school as a reliable source of information within the aviation community. This not only enhances your flight school's credibility but also increases its visibility and brand awareness.",
+              "This not only boosts your flight school's credibility but also contributes to off page SEO efforts by attracting inbound links from authoritative news websites, driving referral traffic, and improving search engine rankings.",
+            ],
+          },
+          {
+            icon: "M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z",
+            title: "Directory listings",
+            descriptions: [
+              "Online directories and review platforms are additional ways we utilize for off page SEO. We optimize your flight school's presence on relevant directories and encourage positive reviews from satisfied students.",
+              "This enhances your flight school's online reputation and credibility while also driving targeted traffic to your website. We've helped flight schools get listed on AOPA, Garmin, Foreflight, and their airport's or FBO's websites.",
+              "Other directories include sites like Yelp, Google My Business, and Apple Maps. In addition, we help fix inconsistent name, address, phone number (NAP) on your listings. If your NAP is inconsistent and varied then search engines get confused and it can hurt your SEO.",
+            ],
+          },
+          {
+            icon: "M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z",
+            title: "Social media engagement",
+            descriptions: [
+              "Online directories and review platforms are additional ways we utilize for off page SEO. We optimize your flight school's presence on relevant directories and encourage positive reviews from satisfied students.",
+              "This enhances your flight school's online reputation and credibility while also driving targeted traffic to your website. We've helped flight schools get listed on AOPA, Garmin, Foreflight, and their airport's or FBO's websites.",
+              "Other directories include sites like Yelp, Google My Business, and Apple Maps. In addition, we help fix inconsistent name, address, phone number (NAP) on your listings. If your NAP is inconsistent and varied then search engines get confused and it can hurt your SEO.",
+            ],
+          },
+        ],
+      },
+      afterListParagraphs: [
+        "Off page SEO is the second step in getting your flight school to rank higher. The first step is on page optimization. This is where we optimize the code on your website so that search engines understand your page better.",
+        "Right Rudder Marketing has a proven strategy that consistently gets our clients to the top of the search engines. We have worked with flight schools in some of the most competitive markets in the United States and have developed a strategy that works.",
+      ],
+      button: {
+        text: "Schedule a Call with the Experts",
+        link: "/contact",
+        primary: true,
+      },
+    },
+
+    flyWithUsCTA: {
+      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-primary">Fly with digital marketing</br> experts</span> that know the flight training industry.`,
+      paragraph:
+        "We are a team of pilots that know the flight training industry. Because if your marketing team doesn't know what the pilot training experience is like, then how can they be successful for you? See what we're talking about by booking a call now!",
+      buttons: [
+        {
+          name: "Call Us",
+          link: "tel:1-314-804-1200",
+          primary: false,
+        },
+        {
+          name: "Book a Strategy Call",
+          link: "/contact",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+export default webDesign;
