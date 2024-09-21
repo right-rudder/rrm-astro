8ad00e454fed6287c0143d14b5da966b42ec0d8c
Raul Ospina <raminka13@hotmail.com>
2024-09-21 18:40:12 -0500

    New: facebook ads page

---
+++ src/data/ppcFacebook.js
@@ -0,0 +1,205 @@
+const ppcFacebook = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Facebook Ads - Pay Per Click | Right Rudder Marketing",
+    pageDescription:
+      "Lear how to use Facebook Ads to market your flight school. We create and manage pay-per-click advertising campaigns that drive traffic, leads, and sales to your business. Facebook ads are a great way to get in front of prospective students that are not in your immediate network.",
+    pageKeywords:
+      "Facebook Ads, Pay Per Click, PPC, Google Ads, Flight School Marketing, Flight Training Marketing, Pilot Training Marketing, Aviation Marketing, Flight School Advertising, Pilot Training Advertising, Aviation Advertising, Flight School Leads, Pilot Training Leads, Aviation Leads, Flight School Sales, Pilot Training Sales, Aviation Sales, Flight School Traffic, Pilot Training Traffic, Aviation Traffic",
+
+    //top header
+    header: {
+      imagePath: "/src/assets/trac-vu-dDWsqFJaMfk-unsplash.jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `Running Effective </br><span class="text-accent-300">Facebook Ads</span>`,
+      paragraph:
+        "Facebook ads are a form of PPC (Pay Per Click) advertising. These advertising initiatives can produce quick, fast, and effective results if done right. We can help you get more students for your flight school.",
+      buttons: [
+        {
+          name: "Call Us",
+          link: "tel:1-314-804-1200",
+          primary: false,
+        },
+        {
+          name: "Schedule a Call",
+          link: "/schedule-call",
+          primary: false,
+        },
+      ],
+    },
+
+    //main content
+    whatsPpc: {
+      imagePath: "/src/assets/william-topa-7_4bU5tHRLQ-unsplash.jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      upperHeading: "What is PPC?",
+      heading: "Pay-Per-Click Advertising",
+      description: `Pay per click ads are just like the name implies. For every click on Facebook and Instagram on your ad, you need to pay a fee to Meta. It's a great way to get your flight school in front of targeted prospective students that are not in your immediate network. Whenever you post something on your social media profile, your post will only circulate and be syndicated to mostly people that have liked and followed your page. If you're looking for a way to expand your reach, Facebook ads acts like the classified ads in a newspaper. You can get your business in front of people that have never heard of your flight school.`,
+      paragraphs: [
+        "A big concern with running PPC ads is how much it will cost. For flight schools and aviation businesses, we've seen the cost per click (CPC) hover between $7 dollars and $20 dollars a click depending on the area and keywords chosen.",
+        "You might be running the numbers in your right now. If I get 100 clicks on my ad, then that can cost anywhere from $700 to $2000. Which in turn may make you wonder if it's even worth it or not. The best question to ask yourself is how much is a student pilot worth in my flight school?",
+      ],
+      list: {
+        heading: "Does this sound like you?",
+        items: [
+          {
+            icon: "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z",
+            title: "Stagnant or slow sales.",
+            descriptions: [
+              "You're wondering why you're competitors are enrolling more students with Facebook Ads and you're not, so you're wondering how to compete.",
+            ],
+          },
+          {
+            icon: "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z",
+            title: "Facebook Ads not converting.",
+            descriptions: [
+              "You spend hundreds and thousands of dollars on Facebook ads and boosting posts with inconsistent results. You are getting tired of chasing leads who express interest and then ghost you.",
+            ],
+          },
+          {
+            icon: "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z",
+            title: "Your marketing agency doesn't understand your business.",
+            descriptions: [
+              "You're tired of explaining how the flight training process and your pilot academy works to your marketing agency. Your spending money on advertising and cost per lead keeps going up.",
+            ],
+          },
+        ],
+      },
+      afterListParagraphs: [
+        "We see so many flight schools that go into running a Facebook Ad campaign by themselves, pick a few keywords and stuff them all into one ad group, bid either too high or too low, and then send people directly to their homepage on their website for when people actually click.",
+        "Or they might boost a post from their social media profile in hopes of getting a new lead. This is the wrong way to do it in so many ways.",
+        "The right way to do it is to engineer multiple ad groups and campaigns. Structure keywords, negative keywords, and locations properly to fit your ideal student pilot. Let's get your flight school more students!",
+      ],
+      finalSection: {
+        heading: "How much is a student pilot worth?",
+        descriptions: [
+          "Let's keep going with the 100 clicks example. If out of those 100 leads, you contact all of them and 30 decide to show up to your flight school and do a tour or a discovery flight, you might get 10 or so that commit to coming again and train. A 10% conversion rate which is typical in the flight school market.",
+          " With the $700 to $2000 dollars you spent, you now have 10 new students. Most students who commit to training to at least their private pilot certificate will bring on average $10,000-$15,000 dollars of revenue to your school. So you just paid $700 to $2000 to bring in more than $100,000 in revenue. It's a good investment.",
+        ],
+      },
+    },
+
+    warningCTA: {
+      icon: "M12 9v3.75m0-10.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.25-8.25-3.286Zm0 13.036h.008v.008H12v-.008Z",
+      heading: "Don't waste your money.",
+      description:
+        "Most Facebook Ads campaigns fail because flight school owners fail to understand how the platform actually works to get leads.",
+      button: {
+        text: "Schedule a Call",
+        link: "/schedule-call",
+        primary: true,
+      },
+    },
+
+    rightWay: {
+      imagePath: "/src/assets/andrew-ruiz--ajZ_Xzeqe4-unsplash.jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      upperHeading: "How to do it right",
+      heading: "What's the right way to go about running Facebook Ads?",
+      paragraphs: [
+        "A thing to note about Facebook ads is that when people are using the platform, they aren't actively looking for flight schools or businesses to spend money on. In fact, most people don't want to see any advertising on their feeds.",
+        "They just want to be entertained and watch videos or see pictures of their friends. So when you're running Facebook ads, you need to be very specific about who you're targeting and what you're showing them.",
+        "If you're showing an ad to someone who has never heard of your flight school before, you need to be very clear about what you're offering and why they should care.",
+        "A different approach is needed for someone who has already visited your website or has already shown interest in your flight school. You need to be more aggressive and show them ads that are more likely to get them to take action.",
+      ],
+      list: {
+        items: [
+          {
+            icon: "m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+            title: "Not relevant Headline.",
+            descriptions: [
+              "Doesn't have a headline that matches the user search term (ie. flight school, pilot training, etc.). Headlines are the first thing a user sees and must grab their attention and curiosity.",
+            ],
+          },
+          {
+            icon: "m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+            title: "Unappealing ad copy.",
+            descriptions: [
+              "Doesn't give the viewer an incentive to call now and check your flight school out. A great ad copy will give the viewer a reason to call now and check your flight school out. Either by offering an exiting experience thought flying.",
+            ],
+          },
+          {
+            icon: "m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+            title: "Not relevant Unique Selling Proposition (USP).",
+            descriptions: [
+              "Doesn't give them a reason to choose your flight school over others. You need to have a unique selling proposition that makes your flight school stand out from the rest. This could be your location, your instructors, your aircraft, or your training program.",
+            ],
+          },
+        ],
+      },
+      afterListParagraphs: [
+        "The same goes with the page that your customers go to when clicking your ad. It needs to be relevant. If a customer is searching for “how to become a pilot”, your ad isn't going to work well if the page they land on is your homepage. It needs to be a page that is relevant to the search term.",
+        "If this isn't done correctly you're back to square one. You're spending a lot of money paying more for ads than you need to pay and you're likely getting fewer calls than you should be. If the ad and landing page content doesn't answer the specific question or query they typed into Google's search bar, then you better believe it's unlikely they'll be reaching out to you.",
+        "So what's the right way to go about doing Facebook ads? It's to understand how the Facebook Ads platform works and how to create ads that are relevant to the user that is searching for flight schools in your area. How deep in the funnel are they? Are they just starting to look at flight schools or are they ready to sign up?",
+      ],
+      button: {
+        text: "Schedule a Call with the Experts",
+        link: "/schedule-call",
+        primary: true,
+      },
+    },
+
+    features: {
+      upperHeading: "Our Process",
+      heading: "The Right Rudder Marketing Method",
+      description: `Use lead generation retargeting ads and capture leads that already know about you, but haven't converted into a student pilot yet. Let's get those future pilots in your school!`,
+      features: [
+        {
+          title: "Secure Your Facebook Business Account",
+          description:
+            "We help you protect and setup your Facebook business properties to ensure your business is not compromised or face account bans/restrictions when advertising.",
+        },
+        {
+          title: "Integrate and Automate CRM Processes",
+          description:
+            "We help you migrate your CRM contacts and setup trigger automation to help you optimize your sales process and lead conversion journey.",
+        },
+        {
+          title: "Lead Conversion and Tracking Setup",
+          description:
+            "We setup your Facebook Pixel, and tracking conversion for leads and phone calls on your website and landing pages.",
+        },
+        {
+          title: "Setup Your Ad Campaigns",
+          description:
+            "Our Facebook Meta Ads Flight School Marketing Specialists will set up your Facebook Ad campaign in 72 hours. We will create the ad copy and headlines to get your campaign up and running.",
+        },
+        {
+          title: "Launch The Ad Campaign",
+          description:
+            "We will launch your Facebook Ad campaign and ensure they're producing leads within 24 hours after going live. We will monitor and optimize your ads to ensure they're producing the best results.",
+        },
+        {
+          title: "Ad Campaign Optimization",
+          description:
+            "Right Rudder Marketing will ensure that your campaign is always performing within KPIs, and optimize for your growth! We will provide you with monthly reports and updates on your campaign.",
+        },
+      ],
+      button: {
+        text: "Schedule a Call with the Experts",
+        link: "/schedule-call",
+        primary: true,
+      },
+    },
+    flyWithUsCTA: {
+      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-primary">Fly with digital marketing</br> experts</span> that know the flight training industry.`,
+      paragraph:
+        "We are a team of pilots that know the flight training industry. Because if your marketing team doesn't know what the pilot training experience is like, then how can they be successful for you? See what we're talking about by booking a call now!",
+      buttons: [
+        {
+          name: "Call Us",
+          link: "tel:1-314-804-1200",
+          primary: false,
+        },
+        {
+          name: "Book a Strategy Call",
+          link: "/schedule-call",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+export default ppcFacebook;
