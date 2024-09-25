4ca772802b9a5aad2d5ffd948d1bf3a77ff52dc7
Raul Ospina <raminka13@hotmail.com>
2024-09-25 10:57:33 -0500

    Update: links, styles, add book funnel

--- src/data/homePage.js
+++ src/data/homePage.js
@@ -300,14 +300,13 @@ const homePage = {
           description:
             "Boosting post and creating ads on Facebook to reach more potential students.",
         },
-
-        {
+        /* {
           name: `Media Production<p class="font-normal text-accent-200 text-sm">(Photography & Videography)</p>`,
           link: "/services/media-production",
           icon: "m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z",
           description:
             "We create high-quality photos and videos to showcase your flight school.",
-        },
+        }, */
       ],
     },
 
