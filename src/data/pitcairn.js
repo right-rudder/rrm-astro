16ee86ec6018ab21f6e06e77a7dbf85510e128a5
Raul Ospina <raminka13@hotmail.com>
2024-09-27 17:12:04 -0500

    New: Pitcairn case study

---
+++ src/data/pitcairn.js
@@ -0,0 +1,116 @@
+const pitcairnFlight = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Pitcairn Flight Academy | Case Study | Right Rudder Marketing",
+    pageDescription:
+      "We built and deployed his website, created his Google Business profile, ran their Google Ads campaign, and also introduced our CRM (customer relationship management) software to help streamline their sales operations. They’re Google Business profile went from non-existent to a fully optimized profile with 5 star reviews showcasing their authority and reputation.",
+    pageKeywords:
+      "pitcairn flight academy case study, marketing case study, aviation case study, flight school marketing, aviation marketing, flight training marketing, pilot training marketing, flight school advertising, aviation advertising, flight training advertising, pilot training advertising, flight school lead generation, aviation lead generation, flight training lead generation, pilot training lead generation, flight school seo, aviation seo, flight training seo, pilot training seo, flight school website design, aviation website design, flight training website design, pilot training website design, flight school social media, aviation social media, flight training social media, pilot training social media",
+
+    //top header
+    header: {
+      imagePath: "/src/assets/pitcairn-heritage-field-fly-in.webp",
+      imageAlt: "Pitcairn Heritage Field panoramic view",
+      headerH1: `<span class="text-accent-300">Pitcairn Flight Academy</span></br>Case Study`,
+      paragraph:
+        "We built and deployed his website, created his Google Business profile, ran their Google Ads campaign, and also introduced our CRM (customer relationship management) software to help streamline their sales operations. They’re Google Business profile went from non-existent to a fully optimized profile with 5 star reviews showcasing their authority and reputation.",
+      buttons: [
+        {
+          name: "Get Our Free Book",
+          link: "/resources/flight-school-handbook",
+          primary: false,
+        },
+        {
+          name: "Our Marketing System",
+          link: "/marketing-system",
+          primary: false,
+        },
+      ],
+    },
+
+    //Case Study
+    caseLong: {
+      upperHeading: "Case Study",
+      heading: "Brand Authority and Reputation",
+      descriptions: [
+        "David, the owner of Pitcairn Flight Academy, at FSANA (Flight School Association of North America) in March 2023. He had just started his aviation business and had one plane.",
+        "David’s business was more than just a flight school. David was in charge of an FBO, a maintenance shop, a flight school, and in process of doing a Part 135 charter operation.",
+        "They’ve now grown to have 3 planes (with more on the way), a full flight schedule, and even hosted a junior aviator camp the other week. In a time frame of about a year, they’ve grown to a scalable highly reputable business that trains pilots everyday.",
+        "Right Rudder Marketing still works with David’s team to this day. As they continue to grow and scale, they have new projects on the horizon. There’s some operational and regulatory bottlenecks we’re facing together, but very soon we will start helping David with promoting and marketing his maintenance and private charter Part 135 operations.",
+      ],
+      content1: {
+        title: "The Problem",
+        paragraphs: [
+          "David was a new business owner in the aviation industry and was looking for a way to get his business off the ground. He was looking for a way to get more students, more revenue, and more authority in the industry.",
+          "David was looking for a way to streamline his sales process and get more leads. He was also looking for a way to build reputation in the industry. He was looking for a way to get his business to the next level.",
+        ],
+      },
+      content2: {
+        title: "Our Solution",
+        paragraphs: [
+          "We built and deployed his website, created his Google Business profile, ran their Google Ads campaign, and also introduced our CRM (customer relationship management) software to help streamline their sales operations. We had a very specific strategy in mind for David’s business.",
+          "We developed a marketing strategy around very specific keywords that would attract the right kind of students to his flight school. SEO was a big part of our strategy. On page SEO and off page SEO were both very important to us. Our campaign was very successful and we were able to position Pitcairn Flight Academy at the top on search results.",
+          "We update the website with new content, insights and articles of great relevance to pilots, and also new offerings for pilot students. We also update the Google Business profile with new photos, new posts, and new reviews. We also update the Google Ads campaign with new keywords, new ad copy, and new landing pages.",
+        ],
+      },
+      finalParagraphs: [
+        "Right Rudder Marketing still works with David’s team to this day. As they continue to grow and scale, they have new projects on the horizon. There’s some operational and regulatory bottlenecks we’re facing together, but very soon we will start helping David with promoting and marketing his maintenance and private charter Part 135 operations.",
+        "We are very excited to see what the future holds for Pitcairn Flight Academy. We are very excited to see what the future holds for David and his team. We are very excited to see what the future holds for the aviation industry.",
+      ],
+      imagePath: "/src/assets/david-pitcairn-tonie-tim.jpg",
+      imageAlt:
+        "David from Pitcairn Flight Academy with Tonie and Tim from Right Rudder Marketing",
+      imageCaption: "Ideal Aviation Facility",
+    },
+
+    sopCTA: {
+      imagePath: "/src/assets/multi-engine-airplane-flying-over-mesa-az.webp",
+      imageAlt: "Start creating your playbook with SOPs",
+      heading: `Start Creating your Playbook with SOPs`,
+      description: `Growing a flight school means creating systems and procedures for your team to follow. Creating SOPs to add to your "playbook" is the best way to properly delegate and lead your team. Download our free template to get started in your continuous improvement journey.`,
+      buttons: [
+        {
+          name: "Download Now",
+          link: "/resources/sop-templates",
+          accent: true,
+        },
+        {
+          name: "Learn More",
+          link: "/blog/how-to-create-sops-to-scale-your-flight-school/",
+          accent: false,
+        },
+      ],
+    },
+
+    testimonial: {
+      imagePath: "/src/assets/Marissa-D-Pitcairn.webp",
+      imageAlt: "Marissa from Pitcairn Flight Academy",
+      quote:
+        "Our flight school has been working with Right Rudder for almost a year now. They are an incredible group of people. We are so happy with them. Absolutely 100% if you have a flight school and need to work with them!",
+      name: "Marissa",
+      client: "Pitcairn Flight Academy",
+      rank: "Academy Administrator",
+    },
+
+    flyWithUsCTA: {
+      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-primary">Fly with digital marketing</br> experts</span> that know the flight training industry.`,
+      paragraph:
+        "We are a team of pilots that know the flight training industry. Because if your marketing team doesn't know what the pilot training experience is like, then how can they be successful for you? See what we're talking about by booking a call now!",
+      buttons: [
+        {
+          name: "Get Our Free Book",
+          link: "/resources/flight-school-handbook",
+          primary: false,
+        },
+        {
+          name: "Book a Strategy Call",
+          link: "/schedule-call",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+export default pitcairnFlight;
