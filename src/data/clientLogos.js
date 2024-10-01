6bd5c5ae97f6ccdc93b91247b87abde54264fc89
Tim Jedrek <tim@timjedrek.com>
2024-10-01 00:27:57 -0500

    get marquee going

---
+++ src/data/clientLogos.js
@@ -0,0 +1,10 @@
+const clientLogos = [
+  { url: '/src/assets/NG Logo White Background 2.webp', link: 'https://flyhere.aero/', invert: true },
+  { url: '/src/assets/SFA-logo.webp', link: 'https://www.summitflightacademy.com/', invert: false },
+  { url: '/src/assets/high-tide-aviation-logo-gradation.CFCXi2nO.png', link: 'https://hightideaviation.com/', invert: false },
+  { url: '/src/assets/sun-city-white-text-logo.webp', link: 'https://suncityaviation.com/', invert: false },
+  { url: '/src/assets/pitcairn-logo-400-white.webp', link: 'https://pitcairnflightacademy.com/', invert: false },
+  { url: '/src/assets/simplifly_2.webp', link: 'https://simpliflyco.com/', invert: false }
+];
+
+export default clientLogos;
