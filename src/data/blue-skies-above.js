da7ec5f54b1e0dbc1b4b640d05343e3b687da98b
Tim Jedrek <tim@timjedrek.com>
2024-10-01 01:02:52 -0500

    rearrange components and add bsa

---
+++ src/data/blue-skies-above.js
@@ -0,0 +1,111 @@
+const blueSkiesAbove = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Ideal Aviation - Case Study | Right Rudder Marketing",
+    pageDescription:
+      "When the team at Ideal Aviation reached out to Right Rudder Marketing, they wanted to updated their WordPress website because it was clunky and slow. They were paying other agencies and service providers thousands of dollars but not getting the results they wanted. Right Rudder Marketing has been a game changer for flight schools. We help flight schools leverage the internet and use well executed marketing strategies to take their business out of the clouds and into VFR.",
+    pageKeywords:
+      "flight school marketing, aviation marketing, flight training marketing, pilot training marketing, flight school advertising, aviation advertising, flight training advertising, pilot training advertising, flight school lead generation, aviation lead generation, flight training lead generation, pilot training lead generation, flight school seo, aviation seo, flight training seo, pilot training seo, flight school website design, aviation website design, flight training website design, pilot training website design, flight school social media, aviation social media, flight training social media, pilot training social media",
+
+    //top header
+    header: {
+      imagePath: "/src/assets/ideal-facility.webp",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-accent-300">Ideal Aviation</span></br>Case Study`,
+      paragraph:
+        "When the team at Ideal Aviation reached out to Right Rudder Marketing, they wanted to updated their WordPress website because it was clunky and slow. They were paying other agencies and service providers thousands of dollars but not getting the results they wanted.",
+      buttons: [
+        {
+          name: "Get Our Free Book",
+          link: "/resources/flight-school-handbook",
+          primary: false,
+        },
+        {
+          name: "Our Marketing System",
+          link: "/marketing-system",
+          primary: false,
+        },
+      ],
+    },
+
+    //Case Study
+    caseLong: {
+      upperHeading: "Case Study",
+      heading: "Marketing and Website Design for Ideal Aviation",
+      descriptions: [
+        "Introducing Ideal Aviation. A flight school that relied on word of mouth and needed to take it to the next level.",
+        "We helped Ideal Aviation streamline their processes, understand what to do to reach their goals, and increased their revenue.",
+      ],
+      content1: {
+        title: "The Problem",
+        paragraphs: [
+          "When the team at Ideal Aviation reached out to Right Rudder Marketing, they wanted to updated their WordPress website because it was clunky and slow. They were paying other agencies and service providers thousands of dollars but not getting the results they wanted. They were relying on word of mouth and needed to take it to the next level.",
+          "They were looking for a marketing team that understood the flight training industry and could help them reach their goals.",
+        ],
+      },
+      content2: {
+        title: "Our Solution",
+        paragraphs: [
+          "One of the services Ideal Aviation was paying thousands of dollars for was a quiz form that would generate leads for them. They embedded a link on their website to take their visitors to a webpage off their domain.",
+          "While they were getting a few inquiries, the quiz was not catered to their needs as a flight school and was more of a general template for different industries.",
+          "We coded up a custom sales funnel using the quiz idea which targeted people who had a specific interest in aviation and then streamlined form submissions directly to their CRM.",
+          "We also completely redid their website to load fast and implemented SEO strategies so they can start ranking higher.",
+          "We helped Ideal Aviation streamline their processes, understand what to do to reach their goals, and increased their revenue.",
+        ],
+      },
+      imagePath: "/src/assets/ideal-facility.webp",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      imageCaption: "Ideal Aviation Facility",
+    },
+
+    sopCTA: {
+      imagePath: "/src/assets/multi-engine-airplane-flying-over-mesa-az.webp",
+      imageAlt: "Start creating your playbook with SOPs",
+      heading: `Start Creating your Playbook with SOPs`,
+      description: `Growing a flight school means creating systems and procedures for your team to follow. Creating SOPs to add to your "playbook" is the best way to properly delegate and lead your team. Download our free template to get started in your continuous improvement journey.`,
+      buttons: [
+        {
+          name: "Download Now",
+          link: "/resources/sop-templates",
+          accent: true,
+        },
+        {
+          name: "Learn More",
+          link: "/blog/how-to-create-sops-to-scale-your-flight-school/",
+          accent: false,
+        },
+      ],
+    },
+
+    testimonial: {
+      imagePath: "/src/assets/kim-ideal-aviation.webp",
+      imageAlt: "Kim from Ideal Aviation",
+      quote:
+        "We have been using Right Rudder for the past 6 months and could not be happier! They revamped our website to make it more user friendly, makes consistent updates, and are very responsive to our needs. We have seen a steady increase in web traffic as well as website driven phone calls since starting with Right Rudder. I can not say enough good things about them!",
+      name: "Kim",
+      client: "Ideal Aviation",
+      rank: "Director of Administration",
+    },
+
+    flyWithUsCTA: {
+      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+      imageAlt: "Right Rudder Marketing team on the runway",
+      headerH1: `<span class="text-primary">Fly with digital marketing</br> experts</span> that know the flight training industry.`,
+      paragraph:
+        "We are a team of pilots that know the flight training industry. Because if your marketing team doesn't know what the pilot training experience is like, then how can they be successful for you? See what we're talking about by booking a call now!",
+      buttons: [
+        {
+          name: "Get Our Free Book",
+          link: "/resources/flight-school-handbook",
+          primary: false,
+        },
+        {
+          name: "Book a Strategy Call",
+          link: "/schedule-call",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+export default blueSkiesAbove;
