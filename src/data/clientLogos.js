1da0f431599c695402831df7f5c88fa89d03782b
Tim Jedrek <tim@timjedrek.com>
2024-10-01 04:43:36 -0500

    move to public

--- src/data/clientLogos.js
+++ src/data/clientLogos.js
@@ -1,17 +1,17 @@
 const clientLogos = [
   
-  { url: '/src/assets/NG Logo White Background 2.webp', link: 'https://flyhere.aero/', invert: true },
-  { url: '/src/assets/SFA-logo.webp', link: 'https://www.summitflightacademy.com/', invert: false },
-  { url: '/src/assets/ideal-logo.webp', link: 'https://idealaviationstl.com/', invert: false },
-  { url: '/src/assets/high-tide-aviation-logo-gradation.CFCXi2nO.png', link: 'https://hightideaviation.com/', invert: false },
-  { url: '/src/assets/sun-city-white-text-logo.webp', link: 'https://suncityaviation.com/', invert: false },
-  { url: '/src/assets/pitcairn-logo-400-white.webp', link: 'https://pitcairnflightacademy.com/', invert: false },
-  { url: '/src/assets/blue-skies-reverse-logo.webp', link: 'https://flytheblueskies.com/', invert: false },
-  { url: '/src/assets/red-arrow-logo.webp', link: 'https://www.flyredarrow.com', invert: false },
-  { url: '/src/assets/cirrus-logo.webp', link: 'https://www.cirrusaviation.com/', invert: false },
-  { url: '/src/assets/simplifly_2.webp', link: 'https://simpliflyco.com/', invert: false },
-  { url: '/src/assets/Tampa-Bay-Aviation-Logo.svg', link: 'https://www.tampabayaviation.com/', invert: false },
-  { url: '/src/assets/ignite-flight-academy-logo.webp', link: 'https://igniteflight.com/', invert: false },
+  { url: 'logos/NG Logo White Background 2.webp', link: 'https://flyhere.aero/', invert: true },
+  { url: 'logos/SFA-logo.webp', link: 'https://www.summitflightacademy.com/', invert: false },
+  { url: 'logos/ideal-logo.webp', link: 'https://idealaviationstl.com/', invert: false },
+  { url: 'logos/high-tide-aviation-logo-gradation.CFCXi2nO.png', link: 'https://hightideaviation.com/', invert: false },
+  { url: 'logos/sun-city-white-text-logo.webp', link: 'https://suncityaviation.com/', invert: false },
+  { url: 'logos/pitcairn-logo-400-white.webp', link: 'https://pitcairnflightacademy.com/', invert: false },
+  { url: 'logos/blue-skies-reverse-logo.webp', link: 'https://flytheblueskies.com/', invert: false },
+  { url: 'logos/red-arrow-logo.webp', link: 'https://www.flyredarrow.com', invert: false },
+  { url: 'logos/cirrus-logo.webp', link: 'https://www.cirrusaviation.com/', invert: false },
+  { url: 'logos/simplifly_2.webp', link: 'https://simpliflyco.com/', invert: false },
+  { url: 'logos/Tampa-Bay-Aviation-Logo.svg', link: 'https://www.tampabayaviation.com/', invert: false },
+  { url: 'logos/ignite-flight-academy-logo.webp', link: 'https://igniteflight.com/', invert: false },
   
 ];
 
